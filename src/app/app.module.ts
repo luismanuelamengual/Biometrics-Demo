a84a479aee99b047a31a77bac75fac3729ec4d55
luis.amengual <luis.amengual@globant.com>
2020-05-24 20:32:39 -0300

    Corrección de indentación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {CameraComponent} from './components/camera/camera.component';
 import {AngularMaterialModule} from './app-material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {PictureGetterComponent} from './components/picture-getter/picture-getter.component';
 import {IdentificationComponent} from './pages/identification/identification.component';
 import {BiometricsInterceptor} from './interceptors/biometrics.interceptor';
+import {PictureGetterComponent} from './components/picture-getter/picture-getter.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         IdentificationComponent,
-        CameraComponent,
         PictureGetterComponent
     ],
     imports: [
--- src/app/components/picture-getter/picture-getter.component.ts
+++ src/app/components/picture-getter/picture-getter.component.ts
@@ -1,91 +1,38 @@
-import { Component, ViewChild, ElementRef, Output, EventEmitter, Input } from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Output, ViewChild} from '@angular/core';
 import ImageUtils from 'src/app/utils/image-utils';
 
 @Component({
-  selector: 'app-picture-getter',
-  templateUrl: './picture-getter.component.html',
-  styleUrls: ['./picture-getter.component.scss']
+    selector: 'app-picture-getter',
+    templateUrl: './picture-getter.component.html',
+    styleUrls: ['./picture-getter.component.scss']
 })
-export class PictureGetterComponent {
+export class PictureGetterComponent implements AfterViewInit {
 
-  @ViewChild('video')
-  public video: ElementRef;
+    @Output()
+    pictureCaptured = new EventEmitter();
 
-  @ViewChild('canvas')
-  public canvas: ElementRef;
+    pictureUrl = null;
 
-  @Output()
-  pictureCaptured = new EventEmitter();
-
-  pictureCaptureSupported: boolean;
-  pictureUrl = null;
-
-  @Input()
-  maxPictureWidth = 1280;
-
-  @Input()
-  maxPictureHeight = 720;
-
-  constructor() {
-    const el: any = document.createElement('input');
-    this.pictureCaptureSupported = el.capture != undefined;
-  }
-
-  public ngAfterViewInit() {
-    this.reset();
-  }
-
-  public capture() {
-    if (!this.pictureCaptureSupported) {
-      this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
-      this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
-      const context = this.canvas.nativeElement.getContext('2d');
-      if (this.video.nativeElement.videoWidth < this.maxPictureWidth) {
-        context.drawImage(this.video.nativeElement, 0, 0, this.video.nativeElement.videoWidth, this.video.nativeElement.videoHeight);
-      } else {
-        context.drawImage(this.video.nativeElement, 0, 0, this.maxPictureWidth, this.maxPictureHeight, 0, 0, this.maxPictureWidth, this.maxPictureHeight);
-      }
-      this.setPicture(this.canvas.nativeElement.toDataURL('image/jpeg'));
+    public ngAfterViewInit() {
+        this.reset();
     }
-  }
 
-  public onPictureCaptured(picture) {
-    const img = new Image();
-    img.onload = () => {
-        const imageWidth = img.width;
-        const imageHeight = img.height;
-        const scale = Math.min((this.maxPictureWidth / imageWidth), (this.maxPictureHeight / imageHeight));
-        const iwScaled = imageWidth * scale;
-        const ihScaled = imageHeight * scale;
-        this.canvas.nativeElement.width = iwScaled;
-        this.canvas.nativeElement.height = ihScaled;
-        const context = this.canvas.nativeElement.getContext('2d');
-        context.drawImage(img, 0, 0, iwScaled, ihScaled);
-        this.setPicture(this.canvas.nativeElement.toDataURL('image/jpeg'));
-    };
-    img.src = URL.createObjectURL(picture);
-  }
-
-  public rotateImage(degrees= 90) {
-    if (this.pictureUrl) {
-      ImageUtils.rotateImage(this.pictureUrl, degrees).then((newPictureUrl) => this.setPicture(newPictureUrl));
+    public onPictureCaptured(picture) {
+        this.setPicture(picture);
     }
-  }
 
-  public setPicture(pictureUrl) {
-    this.pictureUrl = pictureUrl;
-    this.pictureCaptured.emit(this.pictureUrl);
-  }
-
-  public reset() {
-    this.pictureUrl = null;
-    if (!this.pictureCaptureSupported) {
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({video: { width: this.maxPictureWidth, height: this.maxPictureHeight } }).then(stream => {
-            this.video.nativeElement.srcObject = stream;
-            this.video.nativeElement.play();
-        });
-      }
+    public rotateImage(degrees = 90) {
+        if (this.pictureUrl) {
+            ImageUtils.rotateImage(this.pictureUrl, degrees).then((newPictureUrl) => this.setPicture(newPictureUrl));
+        }
     }
-  }
+
+    public setPicture(pictureUrl) {
+        this.pictureUrl = pictureUrl;
+        this.pictureCaptured.emit(this.pictureUrl);
+    }
+
+    public reset() {
+        this.pictureUrl = null;
+    }
 }
