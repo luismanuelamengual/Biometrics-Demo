5348471bad1f94085eceaf4b4ab90185f5566c1e
luis.amengual <luis.amengual@globant.com>
2020-05-25 19:36:05 -0300

    Cambio menor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {AngularMaterialModule} from './app-material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {IdentificationComponent} from './pages/identification/identification.component';
 import {DniPictureGetterComponent} from './components/dni-picture-getter/dni-picture-getter.component';
+import {TitleCasePipe} from '@angular/common';
 
 @NgModule({
     declarations: [
@@ -19,6 +20,9 @@ import {DniPictureGetterComponent} from './components/dni-picture-getter/dni-pic
         BrowserAnimationsModule,
         AngularMaterialModule,
         HttpClientModule
+    ],
+    providers: [
+        TitleCasePipe
     ],
     bootstrap: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
--- src/app/pages/identification/identification.component.ts
+++ src/app/pages/identification/identification.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import ImageUtils from 'src/app/utils/image-utils';
 import {environment} from '../../../environments/environment';
+import {TitleCasePipe} from '@angular/common';
 
 @Component({
     selector: 'app-identification',
@@ -17,10 +18,11 @@ export class IdentificationComponent {
     livenessPictures = [];
     livenessSessionRunning = false;
     verificationResults = null;
+    verificationDniResults = null;
     verificationError = null;
     verifying = false;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, public titleCasePipe: TitleCasePipe) {
         this.biometricsUrl = environment.biometricsUrl;
         this.biometricsApiKey = environment.biometricsApiKey;
     }
@@ -46,32 +48,26 @@ export class IdentificationComponent {
         if (!this.verifying) {
             this.verifying = true;
             this.verificationResults = null;
+            this.verificationDniResults = null;
             this.verificationError = null;
-            const selfieBytes = ImageUtils.convertImageToBlob(this.livenessPictures[0]);
-            const documentFrontBytes = ImageUtils.convertImageToBlob(this.documentFrontPicture);
-            const documentBackBytes = ImageUtils.convertImageToBlob(this.documentBackPicture);
-            const formData = new FormData();
-            formData.append('selfie', selfieBytes);
-            formData.append('documentFront', documentFrontBytes);
-            formData.append('documentBack', documentBackBytes);
             try {
-                const verificationResults: any = await this.http.post( `${environment.biometricsUrl}/v1/verify_identity`, formData, {headers: { Authorization: 'Bearer ' + environment.biometricsApiKey}}).toPromise();
-                if (verificationResults.success && verificationResults.data.match) {
-                    this.verificationResults = { match: verificationResults.data.match, similarity: verificationResults.data.similarity };
-                    try {
-                        const formDataDocument = new FormData();
-                        formDataDocument.append('documentFront', documentFrontBytes);
-                        formDataDocument.append('documentBack', documentBackBytes);
-                        const documentResults: any = await this.http.post(`${environment.biometricsUrl}v1/scan_document_data`, formDataDocument, {headers: { Authorization: 'Bearer ' + environment.biometricsApiKey}}).toPromise();
-                        if (documentResults.success && documentResults.data.information) {
-                            this.verificationResults.documentData = documentResults.data.information;
-                        }
-                    } catch (e) {}
-                } else {
-                    this.verificationResults = { match: false };
-                }
+                const selfieBytes = ImageUtils.convertImageToBlob(this.livenessPictures[0]);
+                const documentFrontBytes = ImageUtils.convertImageToBlob(this.documentFrontPicture);
+                const documentBackBytes = ImageUtils.convertImageToBlob(this.documentBackPicture);
+
+                const formData = new FormData();
+                formData.append('selfie', selfieBytes);
+                formData.append('documentFront', documentFrontBytes);
+                formData.append('documentBack', documentBackBytes);
+                this.verificationResults = await this.http.post( `${environment.biometricsUrl}/v1/verify_identity`, formData, {headers: { Authorization: 'Bearer ' + environment.biometricsApiKey}}).toPromise();
+
+                const formDataDocument = new FormData();
+                formDataDocument.append('documentFront', documentFrontBytes);
+                formDataDocument.append('documentBack', documentBackBytes);
+                this.verificationDniResults = await this.http.post(`${environment.biometricsUrl}v1/scan_document_data`, formDataDocument, {headers: { Authorization: 'Bearer ' + environment.biometricsApiKey}}).toPromise();
+
             } catch (e) {
-                this.verificationResults = { match: false };
+                this.verificationError = e.message;
             }
             this.verifying = false;
         }
