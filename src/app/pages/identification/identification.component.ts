da2c3bb7ab6290d5d9857af865e16f1c99e6dd77
luis.amengual <luis.amengual@globant.com>
2020-09-14 12:05:06 -0300

    Cambios estéticos

--- src/app/pages/identification/identification.component.ts
+++ src/app/pages/identification/identification.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import ImageUtils from 'src/app/utils/image-utils';
 import {environment} from '../../../environments/environment';
 import {TitleCasePipe} from '@angular/common';
 import {BiometricsService} from '../../services/biometrics.service';
