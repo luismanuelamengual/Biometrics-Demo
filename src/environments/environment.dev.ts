9fe17b227e7f9a3d9e39925f080e4128fa04da46
luis.amengual <luis.amengual@globant.com>
2020-11-06 12:47:30 -0300

    Subo actualización de demo

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,8 @@
+import { version } from '../../package.json';
+
 export const environment = {
     production: true,
+    version,
     biometricsUrl: '/biometrics',
     biometricsApiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCIsImNsaWVudCI6InRlc3QifQ.5SY_UQaaILYpryU0qNBuPrPTPkF79UhTCNFElXjzUyY'
 };
