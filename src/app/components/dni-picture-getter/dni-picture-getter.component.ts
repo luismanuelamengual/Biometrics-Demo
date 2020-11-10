4442e1d6832ad72e1c386a0e9e0bf802629ceab4
luis.amengual <luis.amengual@globant.com>
2020-11-10 09:23:10 -0300

    Correcciones menores

--- src/app/components/dni-picture-getter/dni-picture-getter.component.ts
+++ src/app/components/dni-picture-getter/dni-picture-getter.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import ImageUtils from 'src/app/utils/image-utils';
 
 @Component({
@@ -8,11 +8,26 @@ import ImageUtils from 'src/app/utils/image-utils';
 })
 export class DniPictureGetterComponent implements AfterViewInit {
 
+    @ViewChild ('canvas', {static: false})
+    public canvas: ElementRef;
+
     @Output()
     dniCaptured = new EventEmitter();
 
+    pictureCaptureSupported: boolean;
     pictureUrl = null;
     cameraOpen = false;
+
+    @Input()
+    maxPictureWidth = 1280;
+
+    @Input()
+    maxPictureHeight = 720;
+
+    constructor() {
+        const el: any = document.createElement('input');
+        this.pictureCaptureSupported = el.capture !== undefined;
+    }
 
     public ngAfterViewInit() {
         this.reset();
@@ -21,6 +36,23 @@ export class DniPictureGetterComponent implements AfterViewInit {
     public onPictureCaptured(picture) {
         this.setPicture(picture);
         this.cameraOpen = false;
+    }
+
+    public onNativePictureCaptured(picture) {
+        const img = new Image();
+        img.onload = () => {
+            const imageWidth = img.width;
+            const imageHeight = img.height;
+            const scale = Math.min((this.maxPictureWidth / imageWidth), (this.maxPictureHeight / imageHeight));
+            const iwScaled = imageWidth * scale;
+            const ihScaled = imageHeight * scale;
+            this.canvas.nativeElement.width = iwScaled;
+            this.canvas.nativeElement.height = ihScaled;
+            const context = this.canvas.nativeElement.getContext('2d');
+            context.drawImage(img, 0, 0, iwScaled, ihScaled);
+            this.setPicture(this.canvas.nativeElement.toDataURL('image/jpeg'));
+        };
+        img.src = URL.createObjectURL(picture);
     }
 
     public rotateImage(degrees = 90) {
