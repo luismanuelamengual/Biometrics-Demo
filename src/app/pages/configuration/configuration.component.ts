39a504bd277a8c984a62996ba2ced1d498631d6d
luis.amengual <luis.amengual@globant.com>
2020-11-26 14:34:45 -0300

    Agrego cambios a la página para configuración

--- src/app/pages/configuration/configuration.component.ts
+++ src/app/pages/configuration/configuration.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BiometricsService} from '../../services/biometrics.service';
-import {environment} from "../../../environments/environment";
+import {environment} from '../../../environments/environment';
 
 @Component({
     selector: 'app-configuration',
@@ -20,14 +20,20 @@ export class ConfigurationComponent implements OnInit {
         this.biometricsApiKey = this.biometrics.getServerApiKey();
     }
 
-    saveChanges() {
-        this.biometrics.setServerUrl(this.biometricsUrl);
-        this.biometrics.setServerApiKey(this.biometricsApiKey);
+    isDataChanged() {
+        return (this.biometricsUrl !== this.biometrics.getServerUrl() || this.biometricsApiKey !== this.biometrics.getServerApiKey());
     }
 
     resetChanges() {
         this.biometricsUrl = environment.biometricsUrl;
         this.biometricsApiKey = environment.biometricsApiKey;
         this.saveChanges();
+    }
+
+    saveChanges() {
+        this.biometrics.setServerUrl(this.biometricsUrl);
+        this.biometrics.setServerApiKey(this.biometricsApiKey);
+        this.biometricsUrl = this.biometrics.getServerUrl();
+        this.biometricsApiKey = this.biometrics.getServerApiKey();
     }
 }
